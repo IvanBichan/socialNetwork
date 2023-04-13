8ef3aff4ac063801a1660f349225b24064650a30
IvanBichan <ivanbichanfe@gmail.com>
2023-04-13 17:50:03 +0300

    feat:add component ProfileStatusWithHooks

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from "./ProfileInfo.module.css";
 import {Preloader} from "../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
+import {ProfileStatusWithHooks} from "./ProfileStatusWithHooks";
 
 
 export const ProfileInfo = (props) => {
@@ -15,7 +16,8 @@ export const ProfileInfo = (props) => {
             </div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} alt={'avatarka'}/>
-                <ProfileStatus  status={props.status} updateStatus={props.updateStatus}/>
+                {/*<ProfileStatus  status={props.status} updateStatus={props.updateStatus}/>*/}
+                <ProfileStatusWithHooks  status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     );
